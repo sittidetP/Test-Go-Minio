ac1848a3fb508795832a40d2fad5f6519786eed5
sittidetP <sittidet@odds.team>
2022-11-16 13:33:09 +0700

    refactor some work to function

--- file-uploader.go
+++ file-uploader.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/url"
+	"time"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -30,8 +33,21 @@ func main() {
 	// Make a new bucket called mybucket
 	bucketName := "mybucket"
 	location := "us-east-1"
+	createBucket(minioClient, ctx, bucketName, location)
 
-	err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+	// Upload the zip file
+	objectName := "COEXODDS.png"
+	filePath := "./COEXODDS.png"
+	// contentType := "application/zip" //zip file
+	contentType := "" //for image file
+	uploadFileToBucket(minioClient, ctx, bucketName, objectName, filePath, contentType)
+
+	// expiryTime := time.Second * 24 * 60 * 60 * 7
+	// getURLObject(minioClient, ctx, bucketName, objectName, expiryTime)
+}
+
+func createBucket(minioClient *minio.Client, ctx context.Context, bucketName string, location string) {
+	err := minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
 	if err != nil {
 		// Check to see if we already own this bucket (which happens if you run this twice)
 		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
@@ -44,12 +60,9 @@ func main() {
 	} else {
 		log.Printf("Successfully created %s\n", bucketName)
 	}
+}
 
-	// Upload the zip file
-	objectName := "Sign.zip"
-	filePath := "./Sign.zip"
-	contentType := "application/zip"
-
+func uploadFileToBucket(minioClient *minio.Client, ctx context.Context, bucketName string, objectName string, filePath string, contentType string) {
 	// Upload the zip file with FPutObject
 	info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
 	if err != nil {
@@ -57,4 +70,19 @@ func main() {
 	}
 
 	log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+	log.Printf("info: %s", info.ETag)
+}
+
+func getURLObject(minioClient *minio.Client, ctx context.Context, bucketName string, objectName string, expiryTime time.Duration) {
+	// Set request parameters for content-disposition.
+	reqParams := make(url.Values)
+	reqParams.Set("response-content-disposition", fmt.Sprintf("attachment; filename=\"%s\"", objectName))
+
+	// Generates a presigned url which expires in a day.
+	presignedURL, err := minioClient.PresignedGetObject(context.Background(), bucketName, objectName, expiryTime, reqParams)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully generated presigned URL", presignedURL)
 }
